01f4c991972b9aff8f72a6a8ca948e59fb697ceb
Курнышов Илья Михайлович <IMKurnyshov@get-lab.ru>
2024-07-03 01:12:34 +0300

    #6 add missed annotations for Book and LibraryUser entity

--- src/main/java/com/example/booklibraryv2/entities/LibraryUser.java
+++ src/main/java/com/example/booklibraryv2/entities/LibraryUser.java
@@ -7,10 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "library_user")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LibraryUser {
   @Id
   @Column(name = "id")
