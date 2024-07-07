58538209d36235a145b1f55d6153411348d43e37
Курнышов Илья Михайлович <IMKurnyshov@get-lab.ru>
2024-07-07 16:59:17 +0300

    #6 change validation field messages in BookDTO

--- src/main/java/com/example/booklibraryv2/dto/BookDTO.java
+++ src/main/java/com/example/booklibraryv2/dto/BookDTO.java
@@ -3,7 +3,6 @@ package com.example.booklibraryv2.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,13 +16,11 @@ import lombok.NoArgsConstructor;
 public class BookDTO {
 
   private Integer id;
-  @NotBlank
-  @Size(min = 1, max = 200, message = "Length shouldn't be lower than 1 and greater than 200")
+  @NotBlank(message = "Name shouldn't be empty!")
+  @Size(max = 200, message = "Length shouldn't be greater than 200!")
   private String name;
-  @NotBlank
-  @Pattern(regexp = "^[A-Z]\\w*(\\s[A-Z]\\.[A-Z]\\.|$|\\s[A-Z]\\.)",
-      message = "Authors designation isn't valid!(Examples: 'Petrov A.V.', 'Lin X.', 'Euclid'")
-  @Size(min = 1, max = 200, message = "Length shouldn't be lower than 1 and greater than 200")
+  @NotBlank(message = "Author designation shouldn't be empty!")
+  @Size(max = 200, message = "Length shouldn't be greater than 200!")
   private String author;
   @Min(value = 0,
       message = "Year of writing shouldn't be lower than 0!")
