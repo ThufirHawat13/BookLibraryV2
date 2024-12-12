da3b32388009cf852905b5b901bc72fb291f11eb
Курнышов Илья Михайлович <IMKurnyshov@get-lab.ru>
2024-12-12 14:28:22 +0300

    remove BookRepository.findByName

--- src/main/java/com/example/booklibraryv2/repositories/BookRepository.java
+++ src/main/java/com/example/booklibraryv2/repositories/BookRepository.java
@@ -2,13 +2,11 @@ package com.example.booklibraryv2.repositories;
 
 import com.example.booklibraryv2.entities.Book;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-  Optional<Book> findByName(String name);
   List<Book> findByNameContains(String searchQuery);
 }
