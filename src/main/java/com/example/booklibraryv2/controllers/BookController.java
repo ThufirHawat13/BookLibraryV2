1731866b7d99d8a92793f2d0639b7a759c1fee55
Курнышов Илья Михайлович <IMKurnyshov@get-lab.ru>
2025-02-02 22:10:45 +0300

    refactor: remove ResponseBody usage in REST-controllers

--- src/main/java/com/example/booklibraryv2/controllers/BookController.java
+++ src/main/java/com/example/booklibraryv2/controllers/BookController.java
@@ -10,7 +10,6 @@ import com.example.booklibraryv2.services.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
